a628dfb2ce5e24f7899fd0dffeaae1019f4bbcfe
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 08:16:41 -0300

    Update Uni4Exe02.java

--- unidade4/Uni4Exe02/src/Uni4Exe02.java
+++ unidade4/Uni4Exe02/src/Uni4Exe02.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Uni4Exe02 {
+    //entrada: valor
+    //processo: se o valor%2 == o entao ele é par, se não ele é impar
+    //saida: Par ou impar
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Numero maior que 0: ");
