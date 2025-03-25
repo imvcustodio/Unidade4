dc4b3c5ee443024a44a25fcbe43f9223e22d6498
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 08:17:37 -0300

    Update Uni4Exe03.java

--- unidade4/Uni4Exe03/src/Uni4Exe03.java
+++ unidade4/Uni4Exe03/src/Uni4Exe03.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Uni4Exe03 {
+    //entrada: num1, num2
+    //processo: se o num1>num2 entao ele é o maior, se não o num2 é o maior
+    //saida: num1 é maior que num2 ou num2 é maior que num1
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Numero 1: ");
