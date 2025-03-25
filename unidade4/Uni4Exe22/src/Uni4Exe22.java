b8a27a4b70730c2b8ed4c1b0609a357a3e6bc346
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 14:42:23 -0300

    Update Uni4Exe22.java

--- unidade4/Uni4Exe22/src/Uni4Exe22.java
+++ unidade4/Uni4Exe22/src/Uni4Exe22.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
 public class Uni4Exe22 {
-
+/*entradas: titulacao
+    processo: se titulacao == 1 curso = Bacharel em ciencia da computação, se titulacao == 2 curso == Licenciado em computação, se  titulacao == 3 curso == Bacharel em sistemas da informação. 
+    saida: curso
+     */
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Qual titulação deseja, (1/2/3):  "); 
