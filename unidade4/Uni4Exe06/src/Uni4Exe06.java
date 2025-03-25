c58dc70e38ccac6176643ab9757df6751feaca37
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 08:48:40 -0300

    Update Uni4Exe06.java

--- unidade4/Uni4Exe06/src/Uni4Exe06.java
+++ unidade4/Uni4Exe06/src/Uni4Exe06.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 
 public class Uni4Exe06 {
+        //entrada: letter
+        //processo: se letter = F imprima Feminino
+        //senao se letter = M imprima masculino
+        //senao se letter = I imprima Não informado
+        //senao imprima Entrada incorreta
+        //saida: Feminino, masculino, Não informado ou Entrada incorreta
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String letter = scanner.nextLine();
+        char l = letter.toUpperCase().charAt(0);
 
-
-            if (letter.equalsIgnoreCase("F")) {
+            if (l == 'F') {
                 System.out.println("feminino"); 
-            }else if (letter.equalsIgnoreCase("M")) {
+            }else if (l == 'M') {
                 System.out.println("Masculino");
-            }else if (letter.equalsIgnoreCase("I")) {
+            }else if (l == 'I') {
                 System.out.println("Não informado");
             }else{
                 System.out.println("Entrada incorrerta");
             }
 
-        
+            System.out.println("Fim");
         scanner.close();
     }
 }
+
