ed9c63e60fa33492913fffc79aab02bae1b8d37a
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 14:19:42 -0300

    Update Uni4Exe18.java

--- unidade4/Uni4Exe18/src/Uni4Exe18.java
+++ unidade4/Uni4Exe18/src/Uni4Exe18.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+/*entrada:diaVencimento, diaPagamento, prestacao
+processo: se o diaPagamento<diaVencimento, a valor da prestação recebe um desconto de 10% e a prestação e subtraida pelo valor do desconto,
+se nao se diaPagamento + 5>diaVencimento, diaPagamento -=diaVencimento para calcular quantos dias atrasou, e apos isso calculamos  um acrescento 
+na prestação de 2% cada dia de atraso,desconto = (float)(diaPagamento*2) ,prestação = prestação+desconto;
+saida: prestacao
+*/
 public class Uni4Exe18 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
@@ -8,18 +13,18 @@ public class Uni4Exe18 {
         System.out.print("Informe o dia do pagamento :  "); 
         int diaPagamento = scanner.nextInt();
         System.out.print("Informe o valor da prestação :  "); 
-        float prestação = scanner.nextFloat();
+        float prestacao = scanner.nextFloat();
         float desconto = 0;
         
         if (diaPagamento<diaVencimento) {
             desconto = (float) (prestação *0.10);
-            prestação = prestação - desconto;
-            System.out.printf("O pagamento esta em dia. O valor da prestação = R$%.2f",prestação);
+            prestacao = prestacao - desconto;
+            System.out.printf("O pagamento esta em dia. O valor da prestação = R$%.2f",prestacao);
         }else if (diaPagamento + 5>diaVencimento) {
             diaPagamento -=diaVencimento;
             desconto = (float)(diaPagamento*2);
             prestação = prestação+desconto;
-            System.out.printf("O pagamento esta atrasado. Multa de 2(porcento) por dia de atraso. Valor da prestação = R$%.1f",prestação);
+            System.out.printf("O pagamento esta atrasado. Multa de 2(porcento) por dia de atraso. Valor da prestação = R$%.1f",prestacao);
         }
             
     
