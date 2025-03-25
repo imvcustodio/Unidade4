3ad18cd9e5a9ac70de2ca7a13cfe4d31e1d19357
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 10:26:05 -0300

    Update Uni4Exe12.java

--- unidade4/Uni4Exe12/src/Uni4Exe12.java
+++ unidade4/Uni4Exe12/src/Uni4Exe12.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Uni4Exe12 {
+     /*entrada: lado1, lado2 e lado3
+processo:verificar se os lados sao menores que a soma dos outros 2 lados caso nao seja imprima Não formam um triangulo,mas caso ele respeite as somas verifique
+se todos os lados forem iguais ele e equilatero,  se tiver 2 lados iguais
+e um diferente dos demais ele é isosceles e caso sejam todos diferentes ele é escaleno
+saida: equilatero, isosceles, escaleno ou Não formam um triangulo*/
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("lado1");
