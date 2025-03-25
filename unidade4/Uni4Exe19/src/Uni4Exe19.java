f4e4248d216d540baaf5589f608c7e14d40be323
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 14:26:15 -0300

    Update Uni4Exe19.java

--- unidade4/Uni4Exe19/src/Uni4Exe19.java
+++ unidade4/Uni4Exe19/src/Uni4Exe19.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Uni4Exe19 {
+    /*entrada:pointX, pointY
+processo:se o pointX == 0 e o pointY==0 imprima Quadrante 0, se nao se pointX > 0 e o pointY>0 imprima Quadrante 1,se nao se pointX < 0 e o pointY>0 imprima Quadrante 2
+se nao se pointX < 0 e o pointY < 0 imprima Quadrante 3, se nao imprima Quadrante 4
+saida: Quadrante 1, Quadrante  2, Quadrante 3, Quadrante 4
+*/
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print("X:  "); 
