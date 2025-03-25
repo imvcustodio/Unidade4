d87bd749e600889d71fe4945d7df738183af87aa
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 08:55:55 -0300

    Update Uni4Exe07.java

--- unidade4/Uni4Exe07/src/Uni4Exe07.java
+++ unidade4/Uni4Exe07/src/Uni4Exe07.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Uni4Exe07 {
+        //entrada: Peso
+        //processo: se o peso for menor ou igual a 50 valor a pagar = 0.45, se nao calcular o pesoExcedido =  peso - 50;
+        //qtdAdicional = (pesoExcedido/20)+1 , valorPagar = 0.45f + 0.45f * qtAdicional;
+        //saida:valorPagar
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Entre com o Peso da carta"); 
