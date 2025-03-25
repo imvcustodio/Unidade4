c79da1319118c503f7d77ab10e8c7c0aea51b340
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 10:07:09 -0300

    Update Uni4Exe11.java

--- unidade4/Uni4Exe11/src/Uni4Exe11.java
+++ unidade4/Uni4Exe11/src/Uni4Exe11.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Uni4Exe11 {
+    /*entrada: irmao1, irmao2 e irmao3
+processo: se irmao1 == irmao2 && irmao1 == imrao3 imprima TRIGEMEOS
+senao se irmao1 == irmao2 || irmao1 == irmao3 || irmao2 == irmao3 imprima GEMEOS
+senao imprima IRMAOS pois se não é nem 3 iguais ou 2 iguais são todos diferentes.
+saida: TRIGEMEOS, GEMEOS OU IRMAOS*/
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         
