cb0ba399cb78d3402cc7d139a5ad8f8f3dc6d488
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 10:55:25 -0300

    Update Uni4exe15.java

--- unidade4/Uni4Exe15/src/Uni4exe15.java
+++ unidade4/Uni4Exe15/src/Uni4exe15.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Uni4exe15 {
+      /*entrada: mes
+    processo: se o mes de admissao for menor ou igual a 12 ira receber reajuste de 5%, caso mes de admissao seja de 13 a 48 recebe eajuste de 7%
+    se nao imprima reajuste não informado
+    saida:O funcionario ira receber 5% de reajuste, O funcionario ira receber 7% de reajuste ou Reajuste não informado*/
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Mes");
