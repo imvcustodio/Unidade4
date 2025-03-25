2b3ac32566eadad1896697857baa6326e810eb08
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-25 14:32:20 -0300

    Update Uni4Exe20.java

--- unidade4/Uni4Exe20/src/Uni4Exe20.java
+++ unidade4/Uni4Exe20/src/Uni4Exe20.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Uni4Exe20 {
+       /*entrada:nota1, nota2,nota3 e notaExercicios
+processo:media = (nota1+nota2*2+nota3*3+notaExercicios)/7; conceito recebe o valor de, se a media >=9 imprima A, se a media >=7.5 && media<9 imprima B, se a media>=6 &&media<7.5 imprima B
+se media>=4 && media<6 imprima C, se media>=4 && media<6 imprima D, se nao imprima D, se aprovacao recebe o valor de media>=6 imprima aprovado,se nao imprima reprovado
+saida: media, conceito, aprovacao
+*/
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nota 1:  "); 
