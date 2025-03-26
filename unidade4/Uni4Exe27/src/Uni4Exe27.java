8df2df32dfebc5cb7b1db7cf89468e8e3f37cacc
Isac Marlei Vicente Custódio <isac28011986@gmail.com>
2025-03-26 11:13:00 -0300

    Update Uni4Exe27.java

--- unidade4/Uni4Exe27/src/Uni4Exe27.java
+++ unidade4/Uni4Exe27/src/Uni4Exe27.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni4Exe27 {
@@ -22,28 +23,49 @@ public class Uni4Exe27 {
         //aqui acima e pra calcular o total de tempo em minutos
         
         // Arredondamento de horas
-        int horasCobradas = (totalMinutos + 29) / 60;
+        int horas = totalMinutos/60;
+        int minRest = totalMinutos%60;
         // aqui ele soma 29 para que qualquer numero que seja acima de 30 somado com 29 da pra dividir por 60 que no caso arredondaria para 1 hr 
-        if (horasCobradas == 0) {
-            horasCobradas = 1;
-        }
+        
         //aqui ele pega e ve se a hora arredondada  for == 0 ele coloca ela como 1 pq independente se ficar 10 ou 50 minutos ele tem que cobrar uma hora no minimo.
+        float valorCobrar = 0;
+        int horacobrada = horas;
+      
+
+       if (minRest>29) {
+        horas += 1;
+        horacobrada = horas;
+       }else if (minRest<30) {
+        horacobrada = horas;
+       }
+
+       DecimalFormat df = new DecimalFormat("#0.00");
+     
+        switch (horacobrada) {
+            case 1:
+                valorCobrar = horacobrada * 5;
+                break;
+            case 2:
+                valorCobrar = horacobrada * 5;
+                break;
+            case 3:
+                valorCobrar = (float) (10 + (horacobrada - 2) * 7.5);
+                break;
+            case 4:
+                valorCobrar = (float) (10 + (horacobrada - 2) * 7.5);
+                break;
+            default:
+                valorCobrar = (25 + (horacobrada-4)*10);
+                break;
+        }
+        System.out.println("R$ "+df.format(valorCobrar));
         
-        // Cálculo do preço
-        double preco;// var do prço
-        if (horasCobradas <= 2){ 
-        preco = horasCobradas * 5.00;// se a horacobrada for <= 2 entao preco recebe horas cobradas vezes 5 que e o valor para as 2 primeiras horas
-        }else if (horasCobradas <= 4){ 
-        preco = 10.00 + (horasCobradas - 2) * 7.50;// se horacobrada<= 4 entao preço recebe 10 das duas primeiras horas cobradas no caso as 4 -2 para tiras as 2 primeiras ja cobradas no + 10       
-        }else{
-        preco = 25.00 + (horasCobradas - 4) * 10.00; // aqui ele usa o valor 25 para denominar os 10 das 2 primeiras horas com os 15 das 3 e 4 hora pega horas cobradas - 4 pra tiras as horas que ja estao no 25 e entao multiplica o resto por 10 que e o valor do estacionamento para qm passar de 5 horas
-        
-        } 
-        System.out.println("Tempo estacionado: " + (totalMinutos / 60) + "h " + (totalMinutos % 60) + "min");
-        System.out.println("Horas cobradas: " + horasCobradas);
-        System.out.printf("Valor a pagar: R$ %.2f\n", preco);
+
 
         scanner.close();
+
+    }
+}
 
     }
 }
